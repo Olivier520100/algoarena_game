f1d38a04259d657da1e6c6bf5e89582b064202fd
Olivier Saint-Vincent <olivier520100@gmail.com>
2024-01-31 09:56:00 -0500

    Game engine base

---
+++ Game Engine/gamebase.py
@@ -0,0 +1,33 @@
+class Map():
+    def __init__(self) -> None:
+        pass
+
+class Game():
+    def __init__(self):
+        pass
+
+class Team():
+    def __init__(self):
+        pass
+
+# Game objects have subclasses which would be buildings or units:
+
+class GameObject():
+    def __init__(self):
+        pass
+
+
+class Units(GameObject):
+    def __init__(self):
+        pass
+
+
+class Building(GameObject):
+    def __init__(self):
+        pass
+    
+
+
+
+
+
